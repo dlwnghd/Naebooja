7448fbff070ac065dab9c6620e5a7aa336a17979
zinseo964 <wlstj9964@gmail.com>
2023-02-22 15:43:22 +0900

    [create] create transaction method in TransactionRepository interface -> (CRUD and findBy*)

--- NaebooJa/src/main/java/com/lec/spring/repository/TransactionRepository.java
+++ NaebooJa/src/main/java/com/lec/spring/repository/TransactionRepository.java
@@ -3,25 +3,30 @@ package com.lec.spring.repository;
 import com.lec.spring.domain.Transaction;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TransactionRepository {
 
     // 자산 입력
-
+    int save(Transaction transaction);
 
     // 자산 수정
+    int update(Transaction transaction);
 
     // 자산 삭제
+    int delete(Transaction transaction);
 
-    // 특정 id (PK)의 자산 보기 (transaction id)
-    Transaction findById(Long id);
+    // 특정 id (PK)의 자산 전체 보기 (transaction id)
+    List<Transaction> findById(Long id);
 
     // 특정 user(FK) 의 자산 일괄 보기(user_id)
+    List<Transaction> findByUserId(Long id);
 
     // 특정 user 의 특정 거래 내역 보기 (user_id, transaction_type)
+    List<Transaction> findByType(Long id, String type);
 
     // 특정 user 의 특정 기간 내 거래 전체 보기 (user_id, regDate)
-
-    // 특정 user 의 특정 일자의 특정 거래내역 보기 (user_id, transaction_type, regDate)
-
+    List<Transaction> findByDate(Long id, LocalDateTime regDate);
 }
